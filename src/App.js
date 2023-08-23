ab2361b4ff1cd7c5197d0bf15d5a31fc40fb2bdd
Void <shivanshu63073@gmail.com>
2023-08-23 22:59:44 +0530

    search movie title change

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies("Matrix");
+    searchMovies("The hack");
   }, []);
 
   const searchMovies = async (title) => {
